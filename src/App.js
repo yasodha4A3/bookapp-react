3d89cf845a558ce8cdc1965fb143a2a6f7de3ab9
yashoda4A3 <ssdvy.p@gmail.com>
2018-04-12 22:25:10 +0530

    Login form with Clientside Validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
 import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 
-const App = () =>(
-  <div>
-  <Route path="/" exact component={ HomePage }/>
-  <Route path="/login" exact component={ LoginPage }/>
-</div>
+const App = () => (
+  <div className="container">
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
+  </div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
